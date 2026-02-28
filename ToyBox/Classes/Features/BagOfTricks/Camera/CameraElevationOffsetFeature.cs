8039a84ff05ab3c3bdd93cd8521539fc2a9b30b5
TadaEXE <36825573+TadaEXE@users.noreply.github.com>
2026-02-28 02:03:58 +0100

    Fix cam offset is now also apllied when following character

--- ToyBox/Classes/Features/BagOfTricks/Camera/CameraElevationOffsetFeature.cs
+++ ToyBox/Classes/Features/BagOfTricks/Camera/CameraElevationOffsetFeature.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 namespace ToyBox.Features.BagOfTricks.Camera;
 
 [HarmonyPatch, ToyBoxPatchCategory("ToyBox.Features.BagOfTricks.Camera.CameraElevationOffsetFeature")]
-public partial class CameraElevationOffsetFeature : FeatureWithPatch {
+public partial class CameraElevationOffsetFeature : FeatureWithPatch
+{
     private static bool m_IsEnabled = false;
-    public override ref bool IsEnabled {
-        get {
+    public override ref bool IsEnabled
+    {
+        get
+        {
             m_IsEnabled = Settings.CameraElevationOffset != 0f;
             return ref m_IsEnabled;
         }
     }
-    public ref float Value {
-        get {
+    public ref float Value
+    {
+        get
+        {
             return ref Settings.CameraElevationOffset;
         }
     }
-    public override void OnGui() {
-        using (HorizontalScope()) {
-            if (UI.Slider(ref Value, -10f, 100f, 0f, 0, null, null, AutoWidth(), GUILayout.MinWidth(50), GUILayout.MaxWidth(150))) {
-                if (IsEnabled) {
+    public override void OnGui()
+    {
+        using (HorizontalScope())
+        {
+            if (UI.Slider(ref Value, -10f, 10f, 0f, 2, null, null, AutoWidth(), GUILayout.MinWidth(50), GUILayout.MaxWidth(150)))
+            {
+                if (IsEnabled)
+                {
                     Enable();
-                } else {
+                }
+                else
+                {
                     Disable();
                 }
             }
@@ -37,13 +48,22 @@ public partial class CameraElevationOffsetFeature : FeatureWithPatch {
     [LocalizedString("ToyBox_Features_BagOfTricks_Camera_CameraElevationOffsetFeature_Description", "Adds a flat modifier to the height of the camera above the ground.")]
     public override partial string Description { get; }
 
-    protected override string HarmonyName {
-        get {
+    protected override string HarmonyName
+    {
+        get
+        {
             return "ToyBox.Features.BagOfTricks.Camera.CameraElevationOffsetFeature";
         }
     }
     [HarmonyPatch(typeof(CameraRig), nameof(CameraRig.PlaceOnGround2)), HarmonyPostfix, HarmonyPriority(Priority.HigherThanNormal)]
-    private static void CameraRig_PlaceOnGround2_Patch(ref Vector3 __result) {
+    private static void CameraRig_PlaceOnGround2_Patch(ref Vector3 __result)
+    {
         __result.y += Settings.CameraElevationOffset;
+    }
+
+    [HarmonyPatch(typeof(CameraRig), nameof(CameraRig.ScrollTo)), HarmonyPrefix, HarmonyPriority(Priority.HigherThanNormal)]
+    private static void CameraRig_ScrollTo_Patch(ref Vector3 position)
+    {
+        position.y += Settings.CameraElevationOffset;
     }
 }
