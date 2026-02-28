c21da6ebc1958da10a136c0745d12a503751f2d0
TadaEXE <36825573+TadaEXE@users.noreply.github.com>
2026-02-28 02:06:46 +0100

    Add physical zoom as alternative to fov

--- ToyBox/Classes/Features/BagOfTricks/Camera/FOVMultiplierFeature.cs
+++ ToyBox/Classes/Features/BagOfTricks/Camera/FOVMultiplierFeature.cs
@@ -5,29 +5,43 @@ namespace ToyBox.Features.BagOfTricks.Camera;
 
 [IsTested]
 [HarmonyPatch, ToyBoxPatchCategory("ToyBox.Features.BagOfTricks.Camera.FOVMultiplierFeature")]
-public partial class FOVMultiplierFeature : FeatureWithPatch {
+public partial class FOVMultiplierFeature : FeatureWithPatch
+{
     private static bool m_IsEnabled = false;
-    public override ref bool IsEnabled {
-        get {
+
+    public override ref bool IsEnabled
+    {
+        get
+        {
             m_IsEnabled = Settings.FOVMultiplierSetting != 1f;
             return ref m_IsEnabled;
         }
     }
-    public ref float Value {
-        get {
+    public ref float Value
+    {
+        get
+        {
             return ref Settings.FOVMultiplierSetting;
         }
     }
+
+
     [LocalizedString("ToyBox_Features_BagOfTricks_Camera_FOVMultiplierFeature_Name", "Field Of View")]
     public override partial string Name { get; }
-    [LocalizedString("ToyBox_Features_BagOfTricks_Camera_FOVMultiplierFeature_Description", "Changes the min/max FoV (you can scroll further in and out)")]
+    [LocalizedString("ToyBox_Features_BagOfTricks_Camera_FOVMultiplierFeature_Description", "Changes the min/max FOV (you can scroll further in and out)")]
     public override partial string Description { get; }
-    public override void OnGui() {
-        using (HorizontalScope()) {
-            if (UI.Slider(ref Value, 0.4f, 5f, 1f, 2, null, null, AutoWidth(), GUILayout.MinWidth(50), GUILayout.MaxWidth(150))) {
-                if (IsEnabled) {
+    public override void OnGui()
+    {
+        using (HorizontalScope())
+        {
+            if (UI.Slider(ref Value, 0.4f, 5f, 1f, 2, null, null, AutoWidth(), GUILayout.MinWidth(50), GUILayout.MaxWidth(150)))
+            {
+                if (IsEnabled)
+                {
                     Enable();
-                } else {
+                }
+                else
+                {
                     Disable();
                 }
             }
@@ -37,17 +51,21 @@ public partial class FOVMultiplierFeature : FeatureWithPatch {
             UI.Label(Description.Green());
         }
     }
-    protected override string HarmonyName {
-        get {
+    protected override string HarmonyName
+    {
+        get
+        {
             return "ToyBox.Features.BagOfTricks.Camera.FOVMultiplierFeature";
         }
     }
     [HarmonyPatch(typeof(CameraZoom), nameof(CameraZoom.FovMin), MethodType.Getter), HarmonyPostfix]
-    private static void CameraZoom_getPhysicalZoomMin_Patch(ref float __result) {
+    private static void CameraZoom_getFovMin_Patch(ref float __result)
+    {
         __result /= GetInstance<FOVMultiplierFeature>().Value;
     }
     [HarmonyPatch(typeof(CameraZoom), nameof(CameraZoom.FovMax), MethodType.Getter), HarmonyPostfix]
-    private static void CameraZoom_getPhysicalZoomMax_Patch(ref float __result) {
+    private static void CameraZoom_getFovMax_Patch(ref float __result)
+    {
         __result *= GetInstance<FOVMultiplierFeature>().Value;
     }
     [HarmonyPatch(typeof(CameraZoom), nameof(CameraZoom.TickZoom)), HarmonyPrefix]
